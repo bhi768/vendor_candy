aexmod-tmp/platform_vendor_syberia

Ship Google Sans font family as overlay
This adds support for custom fonts starting with Google Sans, making it an opt-in overlay

Change-Id: Id281d447d9d581c408196abd16b8fed60b6682f8
Signed-off-by: DennySPB <dennyspb@gmail.com>

config/common.mk
@@ -86,6 +86,11 @@ PRODUCT_COPY_FILES += \
     vendor/syberia/config/permissions/privapp-permissions-custom.xml:system/product/etc/permissions/privapp-permissions-custom.xml \
     vendor/syberia/prebuilt/common/etc/privapp-permissions-turbo.xml:system/etc/permissions/privapp-permissions-turbo.xml
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/pa/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/syberia/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 
 # Markup libs
 PRODUCT_COPY_FILES += \

bhi768/vendor_candy

config/common.mk
@@ -94,6 +94,12 @@ PRODUCT_COPY_FILES += \
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/candy/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
+    vendor/candy/prebuilt/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+
 # Sysconfigs
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/etc/sysconfig/candy-power-whitelist.xml:system/etc/sysconfig/candy-power-whitelist.xml \